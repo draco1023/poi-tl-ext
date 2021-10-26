62cc64b3eaf3d667d58e90f2db0a8355fdd17bf6
Draco <draco1023@gmail.com>
2021-10-26 10:58:51 +0800

    Extract html render configuration and support show default table borders if the table inside a table cell

---
+++ src/main/java/org/ddr/poi/html/HtmlRenderConfig.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 - 2021 Draco, https://github.com/draco1023
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ddr.poi.html;
+
+import org.ddr.poi.html.util.CSSLength;
+
+/**
+ * @author Draco
+ * @since 2021-10-26
+ */
+public class HtmlRenderConfig {
+    private String globalFont;
+
+    private CSSLength globalFontSize;
+    private int globalFontSizeInHalfPoints;
+
+    private boolean showDefaultTableBorderInTableCell;
+
+    /**
+     * @return global font family
+     */
+    public String getGlobalFont() {
+        return globalFont;
+    }
+
+    public void setGlobalFont(String globalFont) {
+        this.globalFont = globalFont;
+    }
+
+    /**
+     * @return global font size
+     */
+    public CSSLength getGlobalFontSize() {
+        return globalFontSize;
+    }
+
+    public void setGlobalFontSize(CSSLength globalFontSize) {
+        this.globalFontSize = globalFontSize;
+        globalFontSizeInHalfPoints = globalFontSize == null ? 0 : globalFontSize.toHalfPoints();
+    }
+
+    public int getGlobalFontSizeInHalfPoints() {
+        return globalFontSizeInHalfPoints;
+    }
+
+    /**
+     * @return whether to show default table borders if the table inside a table cell
+     */
+    public boolean isShowDefaultTableBorderInTableCell() {
+        return showDefaultTableBorderInTableCell;
+    }
+
+    public void setShowDefaultTableBorderInTableCell(boolean showDefaultTableBorderInTableCell) {
+        this.showDefaultTableBorderInTableCell = showDefaultTableBorderInTableCell;
+    }
+}
