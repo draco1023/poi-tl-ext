790209587bd4fc8a982861cd24575eb3b8254b3d
Draco <draco1023@gmail.com>
2022-07-05 17:28:25 +0800

    fixes #46 Synchronize runs in paragraph with xml

---
+++ src/main/java/org/ddr/poi/html/util/XWPFParagraphRuns.java
@@ -0,0 +1,52 @@
+package org.ddr.poi.html.util;
+
+import org.apache.poi.xwpf.usermodel.IRunElement;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+/**
+ * Wrapper class for XWPFParagraph runs
+ *
+ * @author Draco
+ * @since 2022-07-05
+ */
+public class XWPFParagraphRuns {
+    private static Field runsField;
+    private static Field irunsField;
+
+    static {
+        try {
+            runsField = XWPFParagraph.class.getDeclaredField("runs");
+            irunsField = XWPFParagraph.class.getDeclaredField("iruns");
+        } catch (NoSuchFieldException ignored) {
+        }
+        runsField.setAccessible(true);
+        irunsField.setAccessible(true);
+    }
+
+    private List<XWPFRun> runs;
+    private List<IRunElement> iruns;
+
+    @SuppressWarnings("unchecked")
+    public XWPFParagraphRuns(XWPFParagraph paragraph) {
+        try {
+            runs = (List<XWPFRun>) runsField.get(paragraph);
+            iruns = (List<IRunElement>) irunsField.get(paragraph);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Remove run at position without modifying xml
+     *
+     * @param pos index of run
+     */
+    public void remove(int pos) {
+        runs.remove(pos);
+        iruns.remove(pos);
+    }
+}
