b7ce89ccc3370bd1c74f1ca5b791d5b62de19d18
Draco <draco1023@gmail.com>
2024-09-13 17:10:29 +0800

    fixes #119 Apply paragraph style properly

--- src/main/java/org/ddr/poi/html/HtmlRenderContext.java
+++ src/main/java/org/ddr/poi/html/HtmlRenderContext.java
@@ -1311,6 +1311,8 @@ public class HtmlRenderContext extends RenderContext<String> {
                 }
 
                 RenderUtils.paragraphStyle(this, xwpfParagraph, cssStyleDeclaration);
+            } else {
+                RenderUtils.paragraphStyle(this, dedupeParagraph, cssStyleDeclaration);
             }
         }
 
